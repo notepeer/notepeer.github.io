995b68e4bfd1dcda545021f5dfe3c82cbf34a147
Sai On Action <gautamsairash@gmail.com>
2025-04-25 11:49:11 +0545

    built prod on 20225-04-25

---
+++ assets/AboutView-DWqX7Zbw.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,a as t,o as a}from"./index-DxgwtyjY.js";const c={},r={class:"about"};function n(_,e){return a(),o("div",r,e[0]||(e[0]=[t("h1",{class:"bg-red-500"},"This is an about page",-1)]))}const d=s(c,[["render",n]]);export{d as default};
